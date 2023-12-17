f4c77323ce7dc3d8bbde00e3d535cf19d3b3dd63
James Aslett <jamesaslett1985@hotmail.com>
2023-12-17 16:06:14 +0000

    Challenge complete

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,15 @@
 import { reactive } from "vue";
 import { StarIcon } from "@heroicons/vue/24/solid";
 import { items } from "./movies.json";
+
 const movies = reactive(items);
+
+function updateRating(newRating, movie) {
+  console.log(`New rating:${newRating}`)
+  console.log(`Old rating:${movie.rating}`)
+  movie.rating = newRating;
+}
+
 </script>
 
 <template>
@@ -17,12 +25,8 @@ const movies = reactive(items);
           <div class="movie-item-title-wrapper">
             <h3 class="movie-item-title">{{ movie.name }}</h3>
             <div class="movie-item-genres-wrapper">
-              <span
-                v-for="genre in movie.genres"
-                :key="`${movie.id}-${genre}`"
-                class="movie-item-genre-tag"
-                >{{ genre }}</span
-              >
+              <span v-for="genre in movie.genres" :key="`${movie.id}-${genre}`" class="movie-item-genre-tag">{{ genre
+              }}</span>
             </div>
           </div>
           <div class="movie-item-description-wrapper">
@@ -32,11 +36,23 @@ const movies = reactive(items);
             <span class="movie-item-rating-text">
               Rating: ({{ movie.rating }}/5)
             </span>
-            <StarIcon
+
+            <!-- CHALLENGE 2: Create buttons for each rating from 1 to 5 and display a star inside -->
+            <!-- CHALLENGE 3: Color the stars based on the movie rating. For example, if the movie rating is 3, color 3 stars yellow and 2 gray -->
+            <!-- CHALLENGE 4: Allow the end user to click on any of the 5 buttons to update the movie rating accordingly -->
+            <!-- CHALLENGE 5: Disable the button for the currently selected rating to prevent the user from giving the same rating twice -->
+            <button @click="updateRating(star, movie)" v-for="(star, index) in 5" :key="`star-${index}`"
+              :class="{ 'movie-item-star-icon-yellow': index < movie.rating, 'movie-item-star-icon-gray': index >= movie.rating }"
+              :disabled="index + 1 === movie.rating">
+              <StarIcon />
+            </button>
+
+            <!-- CHALLENGE 1: Remove the display-only stars that we coded in the last challenge -->
+            <!-- <StarIcon
               v-for="star in movie.rating"
               :key="`star-${star}`"
               class="movie-item-star-icon"
-            />
+            /> -->
           </div>
         </div>
       </div>
